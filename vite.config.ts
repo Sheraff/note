9e34a004184ffb70b79cd525a9640e693b6b1ce5
Sheraff <me@florianpellet.com>
2026-04-05 20:39:30 +0200

    unify vite client & server configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,28 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
-  build: {
-    outDir: 'dist/client',
-    emptyOutDir: true,
+  environments: {
+    client: {
+      consumer: 'client',
+      build: {
+        outDir: 'dist/client',
+        emptyOutDir: true,
+      },
+    },
+    server: {
+      consumer: 'server',
+      build: {
+        ssr: 'server/index.ts',
+        outDir: 'dist/server',
+        emptyOutDir: true,
+        target: 'node24',
+        rolldownOptions: {
+          output: {
+            entryFileNames: 'server.js',
+          },
+        },
+      },
+    },
   },
   clearScreen: false,
   test: {
